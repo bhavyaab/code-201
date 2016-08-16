3a5dd21ad0d149a881ae3e4e57022b05f8857e96
Bhavya Bharti <bhavyab@outlook.com>
2016-08-16 13:52:54 -0700

    udated js file

---
+++ app.js
@@ -0,0 +1,13 @@
+/* multi line comment */
+
+
+'use strict';
+var user = prompt('What is your name?');
+console.log('user name', user)
+
+
+//looking for name brain
+if(user.toLowerCase() === 'brian' || user.toUperCase() === 'BRAIN'){
+ //do some thing..
+
+}
